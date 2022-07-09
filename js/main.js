71a0af19da4252754b724c8c9c79458989b4cf65
laila kamel <laila_kamel@hotmail.com>
2022-07-09 20:15:02 +1000

    media query added

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ let fItem=document.getElementById("first");
 let sItem=document.getElementById("second");
 let prevBtn=document.getElementById("prevBtn");
 let nextBtn=document.getElementById("nextBtn");
+let navbtn=document.getElementById("navBtn");
+let navList=document.getElementById("navList");
 // console.log(imgArr);
 
 prevBtn.addEventListener("click",function(){
@@ -18,3 +20,9 @@ nextBtn.addEventListener("click",function(){
     sItem.style.display="none";
     fItem.style.display="block";
     })
+
+// navbtn.addEventListener("click",function(){
+// navList.style.display="block";
+// navList.style.flexDirection="column";
+// $("navList").show();
+// })
